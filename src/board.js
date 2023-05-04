5c627403a7317fb263c97db2333e78e7de2aad74
TMNTony <121651052+TMNTony@users.noreply.github.com>
2023-05-04 12:48:49 -0500

    Create table

--- src/board.js
+++ src/board.js
@@ -1,5 +1,45 @@
 const board = () => {
-  startLocation = [1, 1];
+  const startLocation = [0, 0];
+  const coordinates = [];
+  const chessTable = document.createElement("table");
+
+  chessTable.setAttribute("class", "center");
+  for (let i = 0; i < 8; i++) {
+    const row = document.createElement("tr");
+    const rowCoord = Math.abs(i - 7);
+    row.textContent = rowCoord;
+    for (let z = 0; z < 8; z++) {
+      const tableCell = document.createElement("td");
+      const colCoord = z;
+      tableCell.textContent = colCoord;
+      if ((i + z) % 2 == 0) {
+        tableCell.setAttribute("class", "cell white");
+        row.appendChild(tableCell);
+        coordinates.push(rowCoord);
+        coordinates.push(colCoord);
+        tableCell.dataset.coordinates = coordinates;
+        coordinates.splice(0, 2);
+      } else {
+        tableCell.setAttribute("class", "cell black");
+        row.appendChild(tableCell);
+        coordinates.push(rowCoord);
+        coordinates.push(colCoord);
+        tableCell.dataset.coordinates = coordinates;
+        coordinates.splice(0, 2);
+      }
+    }
+    chessTable.appendChild(row);
+  }
+
+  const cellNodes = chessTable.querySelectorAll("td");
+  cellNodes.forEach((cellNode) => {
+    if (startLocation.toString() === cellNode.dataset.coordinates) {
+      const knightImg = document.createElement("img");
+      knightImg.src = "./assets/knight/svg";
+      cellNode.appendChild(knightImg);
+    }
+  });
+  document.body.appendChild(chessTable);
 };
 
 export default board;
