c904a3991d5f1c094880d704559c2d714715f95e
TMNTony <121651052+TMNTony@users.noreply.github.com>
2023-05-10 16:10:29 -0500

    Add logic to keep knight moves on board

--- src/algo.js
+++ src/algo.js
@@ -6,6 +6,27 @@ const chessSquare = (x, y) => {
   let predecessor;
 
   const moves = [
-    [2, 1], [2, -1], [-2, 1], [-2, -1], [1, 2], [1, -2], [-1, 2], [-1, -2],
+    [2, 1], [2, -1],
+    [-2, 1], [-2, -1],
+    [1, 2], [1, -2],
+    [-1, 2], [-1, -2],
   ];
+
+  const getPredecessor = () => predecessor;
+  const setOredecessor = (newPredecessor) => {
+    predecessor = predecessor || newPredecessor;
+  };
+
+  const name = () => `${x}, ${y}`;
+
+  const possibleKnightMoves = () => moves
+    .map((offset) => newSquareFrom(offset[0], offset[1]))
+    .filter((square) => square !== undefined);
+
+  const newSquareFrom = (xOffset, yOffset) => {
+    const [newX, newY] = [xPosition + xOffset, yPosition + yOffset];
+    if (newX >= 0 && newX < 8 && newY >= 0 && newY < 8) {
+      return chessSquare(newX, newY);
+    }
+  };
 };
