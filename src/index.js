e5a7f08ef8c6edd6a030d3b1a8bbe214072bef3c
TMNTony <121651052+TMNTony@users.noreply.github.com>
2023-05-10 16:41:30 -0500

    Import knightTravails into index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import "./style.css";
 import board from "./board.js";
+import knightTravails from "./algo";
 
 const controller = (function () {
   board();
+  knightTravails([3, 3], [4, 3]);
 }());
